60d68cd4bfcd1ade30255307bcbdefce61c44616
devino29 <devinwahyu07@gmail.com>
2021-12-08 22:50:35 +0700

    PAS

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home",
+        "nama" => "Devin Wahyu Wijaya",
+    ]);
+
+    
+});
+Route::get('/about', function () {
+    return view('about',[
+        "title" => "About",
+    ]);
+
+    
+});
+Route::get('/portofolio', function () {
+    return view('portofolio', [
+        "title" => "Portofolio",
+    ]);
+
+    
+});
+
+Route::get('/contact', function () {
+    return view('contact', [
+        "title" => "contact",
+    ]);
+
+    
+});
+
